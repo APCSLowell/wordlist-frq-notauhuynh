51dc9555350ca2cf05bbaffbc85790e735bd357c
notauhuynh <142616016+notauhuynh@users.noreply.github.com>
2024-04-03 09:28:36 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -10,13 +10,23 @@ public class WordList
 
   public int numWordsOfLength(int len)
   {
-    /* to be implemented in part (a) */
-    
+    int temp = 0; 
+    for(int i = 0; i < myList.size(); i++){
+      if(myList.get(i).length() == len){
+        temp++;
+      }
+    }
+    return temp;
   }
 
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
+   for(int i = 0; i < myList.size(); i++){
+      if(myList.get(i).length() == len){
+          myList.remove(i);
+          i--;
+      }
+   }
 
   }
 }
